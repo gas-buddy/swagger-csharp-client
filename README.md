c0f5a3016dbdc7ac0a2a8974c2c7e43dd307a5a9
Adam Brodner <abrodner@openstoreloyalty.com>
2016-12-21 14:04:53 -0600

    Added logic to update git repo corresponding to the client

--- README.md
+++ README.md
@@ -8,13 +8,34 @@ There are two main reasons why we need this tool.
 2. We have chosen [swagger-codegen](https://github.com/swagger-api/swagger-codegen) as the c# client generator.  This tool is great, but we need to use a custom `swagger-codegen` template.  We need a custom template since, for example, the default template uses the api host defined in the Swagger specification and then "baked" into the generated client code, which means we would need to regenerate the client for different environments.  For this reason we have created a custom `swagger-codegen` template, which is included in this repo, that causes the generated client to use the api host (and other environment-specific settings) that is defined the .NET configuration file for the generated client.
 
 # Setup and use this swagger-csharp-client
+
+##Requirements
+
 This tool requires maven(http://maven.apache.org/) to be installed on your computer before using it.
+
+## Clone the repository
 
 ```
 git clone git@github.com:gas-buddy/swagger-csharp-client.git
 cd swagger-csharp-client
 npm install
-npm run generate-client [ApiRepoName] [ClientRepoFolder]
 ```
 
-The last step will take several minutes the first time you run it as it needs to clone and build swagger-codegen.
+## Create client repository
+
+If you want to generate a client repository on git or update an existing client repository then follow these steps:
+1. Create a new repository for the client if it doesnt already existing
+  Go to github and add a new repo should be name [ApiRepoName]-client-csharp
+  Make sure it is created with a README as this will create a master branch
+2. Run the following command to generate a client
+
+```
+npm run generate-client [ApiRepoName] [ClientRepoName]
+```
+
+If you just want to generate the code for a client locally then run the following instead:
+```
+npm run generate-client-local [ApiRepoName] [ClientName]
+```
+
+The first time you run the generate-client command in either case it will take several minutes to run as it needs to clone and build swagger-codegen.
--- src/generate.js
+++ src/generate.js
@@ -2,8 +2,9 @@ import childProcess from 'child_process';
 import fs from 'fs';
 import mustache from 'mustache';
 
-const repoNameArg = process.argv[2];
-const outputDirectoryArg = process.argv[3];
+const updateGitArg = process.argv[2];
+const repoNameArg = process.argv[3];
+const clientRepoNameArg = process.argv[4];
 
 // Clones and builds the codegen repo so it can be used locally
 function cloneCodegen() {
@@ -16,7 +17,7 @@ function cloneCodegen() {
 
 // Create script to clone repo api spec
 function cloneRepoApi(repoName) {
-  let script = fs.readFileSync('./src/templates/gitClone.sh.mustache', 'utf-8');
+  let script = fs.readFileSync('./src/templates/cloneApiSwagger.sh.mustache', 'utf-8');
 
   const repoDirectory = `./temp/repo/${repoName}`;
   script = mustache.render(script, { repoName, repoDirectory });
@@ -26,7 +27,7 @@ function cloneRepoApi(repoName) {
     fs.mkdirSync(scriptFolder);
   }
 
-  const scriptPath = `${scriptFolder}/gitClone.sh`;
+  const scriptPath = `${scriptFolder}/cloneApiSwagger.sh`;
   fs.writeFileSync(scriptPath, script);
   childProcess.execSync(`sh ${scriptPath}`);
 
@@ -74,18 +75,93 @@ function skewerCaseStringToPascalCaseString(str) {
   });
 }
 
+// Clones the client repo
+function cloneClient(repoName, repoDirectory) {
+  // eslint-disable-next-line no-console
+  console.log(`Cloning ${repoName}...`);
+
+  let script = fs.readFileSync('./src/templates/cloneClient.sh.mustache', 'utf-8');
+  script = mustache.render(script, { repoName, repoDirectory });
+  const scriptPath = './temp/cloneClient.sh';
+  fs.writeFileSync(scriptPath, script);
+  childProcess.execSync(`sh ${scriptPath}`);
+}
+
+// Pulls the client repo to ensure it is up to date
+function pullClient(repoName, repoDirectory) {
+  // eslint-disable-next-line no-console
+  console.log(`Getting latest on ${repoName}...`);
+
+  let script = fs.readFileSync('./src/templates/pullClient.sh.mustache', 'utf-8');
+  script = mustache.render(script, { repoDirectory });
+  const scriptPath = './temp/pullClient.sh';
+  fs.writeFileSync(scriptPath, script);
+  childProcess.execSync(`sh ${scriptPath}`);
+}
+
+// Returns the commit Id of the api
+function getApiCommitId(apiDirectory) {
+  let script = fs.readFileSync('./src/templates/getCommitId.sh.mustache', 'utf-8');
+  script = mustache.render(script, { repoDirectory: apiDirectory });
+  const scriptPath = './temp/getCommitId.sh';
+  fs.writeFileSync(scriptPath, script);
+  const commitId = childProcess.execSync(`sh ${scriptPath}`);
+  return commitId;
+}
+
+// Checkouts a new branch of the client repo
+function checkoutClient(clientRepoName, clientRepoDirectory, branchName) {
+  if (!fs.existsSync(clientRepoDirectory)) {
+    cloneClient(clientRepoName, clientRepoDirectory);
+  } else {
+    pullClient(clientRepoName, clientRepoDirectory);
+  }
+
+  let script = fs.readFileSync('./src/templates/createBranch.sh.mustache', 'utf-8');
+  script = mustache.render(script, { branchName, repoDirectory: clientRepoDirectory });
+  const scriptPath = './temp/createBranch.sh';
+  fs.writeFileSync(scriptPath, script);
+  childProcess.execSync(`sh ${scriptPath}`);
+}
+
+// Commits the branch and pushes it to the remote
+function commitClient(apiName, clientDirectory, commitId) {
+  const comment = `Auto generated client corresponding to https://github.com/gas-buddy/${apiName}/tree/${commitId}`;
+
+  let script = fs.readFileSync('./src/templates/commitClient.sh.mustache', 'utf-8');
+  script = mustache.render(script, { repoDirectory: clientDirectory, comment });
+  const scriptPath = './temp/commitClient.sh';
+  fs.writeFileSync(scriptPath, script);
+
+  childProcess.execSync(`sh ${scriptPath}`);
+}
+
 // Generate the client code for the swagger doc
-function generateClient(repoName, swaggerFilePath, outputDirectory) {
-  let packageName = skewerCaseStringToPascalCaseString(repoName);
-  packageName = packageName.replace('Api', 'Client');
+function generateClient(repoName, repoDirectory, swaggerFilePath, clientRepoName, updateGit) {
+  const commitId = getApiCommitId(repoDirectory);
+  const branchName = `auto-generated-commit#${commitId}`;
+
+  const outputDirectory = `./../${clientRepoName}`;
+
+  if (updateGit) {
+    checkoutClient(clientRepoName, outputDirectory, branchName);
+  }
+
+  const packageName = skewerCaseStringToPascalCaseString(clientRepoName);
 
   // eslint-disable-next-line no-console
   console.log(`Generating ${packageName}...`);
-  childProcess.execSync(`java -jar ./../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate   -i ${swaggerFilePath}   -l csharp   -o ${outputDirectory}/${packageName} --additional-properties packageName=${packageName} -t ./src/templates`);
+  childProcess.execSync(`java -jar ./../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate   -i ${swaggerFilePath}   -l csharp   -o ${outputDirectory} --additional-properties packageName=${packageName} -t ./src/templates`);
 
   let appConfig = fs.readFileSync('./src/templates/app_test.config.mustache', 'utf-8');
   appConfig = mustache.render(appConfig, { packageName });
-  fs.writeFileSync(`${outputDirectory}/${packageName}/src/${packageName}.Test/app.config`, appConfig);
+  fs.writeFileSync(`${outputDirectory}/src/${packageName}.Test/app.config`, appConfig);
+
+  if (updateGit) {
+    // eslint-disable-next-line no-console
+    console.log(`Pushing branch https://github.com/gas-buddy/${clientRepoName}/tree/${branchName}...`);
+    commitClient(repoName, outputDirectory, commitId);
+  }
 }
 
 // Deletes a folder and all of it's contents
@@ -104,17 +180,20 @@ function deleteFolderRecursive(path) {
 }
 
 // Generates a csharp client from a repo
-function generate(repoName, outputDirectory) {
-  if (repoName && outputDirectory) {
+function generate(repoName, clientRepoName, updateGit) {
+  if (repoName && clientRepoName) {
     cloneCodegen();
     const repoDirectory = cloneRepoApi(repoName);
     const swaggerFilePath = collateSwagger(repoName, repoDirectory);
-    generateClient(repoName, swaggerFilePath, outputDirectory);
+    generateClient(repoName, repoDirectory, swaggerFilePath, clientRepoName, updateGit);
     deleteFolderRecursive('./temp');
+
+    // eslint-disable-next-line no-console
+    console.log(`Succesffully updated ${clientRepoName}`);
   } else {
     // eslint-disable-next-line no-console
-    console.log('You must specify a repository name and an output directory');
+    console.log('You must specify an api repository name and a client respository name');
   }
 }
 
-generate(repoNameArg, outputDirectoryArg);
+generate(repoNameArg, clientRepoNameArg, updateGitArg === 'true');
